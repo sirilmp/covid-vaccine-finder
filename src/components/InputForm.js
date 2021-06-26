58e9f12df341c860345307b26a2a927c0c04481c
sirilmp <sirilmp8@gmail.com>
2021-06-26 12:52:48 +0530

    audio added

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -320,8 +320,15 @@ function InputForm() {
     // Let's check whether notification permissions have already been granted
     else if (Notification.permission === "granted") {
       // If it's okay let's create a notification
-      new Notification("working proprrliy");
-      window.location.reload()
+      // new Notification("working proprrliy");
+      Swal.fire({
+        html:`<audio autoplay>
+        <source src="../images/ag.mp3" type="audio/ogg" >
+        
+      Your browser does not support the audio element.
+      </audio>`
+      })
+      // window.location.reload()
     }
   
     // Otherwise, we need to ask the user for permission
